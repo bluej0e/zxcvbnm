13c5aa6784fe8620bbde55c7cdef4da2d90e6cfd
Rodrigo <rv.rodrigo.viola@gmail.com>
2018-01-08 18:38:50 -0300

    Revert "a"
    This reverts commit 6abc8ade6d21581a3960355a05d87333ff7e8fb1.

---
+++ js/custom_scripts.js
@@ -0,0 +1,123 @@
+  // var $win = $(window),
+  // w = 0,
+  // h = 0,
+  // rgb = [],
+  // getWidth = function() {
+  //   w = $win.width();
+  //   h = $win.height();
+  // };
+  // $win.resize(getWidth).mousemove(function(e) {
+
+  //  rgb = [
+  //  Math.round(e.pageX/w * 255),
+  //  Math.round(e.pageY/h * 255),
+  //  Math.round(((e.pageX + e.pageY) / 2) / ((h+w)/2) / 255)
+  //       // 150`
+  //       ];
+
+
+  //       rgb = [
+  //       Math.round(e.pageX/w * 255),
+  //       Math.round(e.pageY/h * 60)+40,
+  //       Math.round((e.pageX + e.pageY) / (h+w) * 10)+ 45
+  //       // 150`
+  //       ];
+  //       $(".downlogo").css('fill','hsl('+rgb[0]+','+rgb[1]+'%, '+rgb[2]+'%)');
+  //       $(".faro").css('fill','hsl('+rgb[0]+','+rgb[1]+'%, '+rgb[2]+'%)');
+  //       // $(".st0").css('fill','rgb('+rgb.join(',')+')');
+  //     }).resize();
+
+      // ————————————————————— Random BAckground Generator ————————————————————— //
+    //  $(function() {
+    //   var images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg6.jpg'];
+    //   $('.randbg').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    // });
+
+// ————————————————————— TextScramble ————————————————————— //
+
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+const phrases = [
+'SEO',
+'SEM',
+'MARKETING',
+'PERFORMANCE',
+'ANALYTICS',
+'GROWTH HACKING',
+'DESIGN',
+'BRANDING',
+'SOCIAL MEDIA',
+'DIGITAL MARKETING',
+'CONSULTING',
+]
+
+const el = document.querySelector('#textscrambler')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
+
+
+// ————————————————————— MAPA ————————————————————— //
+
+
+// ----- //
--- js/glitch.js
+++ js/glitch.js
@@ -1,1 +1,155 @@
-(function(){"use strict";var textSize=10;var glitcher={init:function(){setTimeout((function(){this.canvas=document.getElementById('stage');this.context=this.canvas.getContext('2d');this.initOptions();this.resize();this.tick();}).bind(this),100);},initOptions:function(){this.width=document.documentElement.offsetWidth;this.height=document.documentElement.offsetHeight;this.textSize=Math.floor(this.width/4);if(this.textSize>this.height){this.textSize=Math.floor(this.height/1.5);}this.font='100 '+this.textSize+'px "1UP"';this.context.font=this.font;this.text='A';this.textWidth=(this.context.measureText(this.text)).width;this.fps=60;this.channel=0;this.compOp='lighter';this.phase=0.0;this.phaseStep=0.9;this.amplitude=0.0;this.amplitudeBase=4.0;this.amplitudeRange=4.0;this.alphaMin=0.9;this.glitchAmplitude=5.0;this.glitchThreshold=0.90;this.scanlineBase=40;this.scanlineRange=40;this.scanlineShift=15;},tick:function(){setTimeout((function(){this.phase+=this.phaseStep;if(this.phase>1){this.phase=0.0;this.channel=(this.channel===2)?0:this.channel+1;this.amplitude=this.amplitudeBase+(this.amplitudeRange*Math.random());}this.render();this.tick();}).bind(this),1000/this.fps);},render:function(){var x0=this.amplitude*Math.sin((Math.PI*2)*this.phase)>>0,x1,x2,x3;if(Math.random()>=this.glitchThreshold){x0*=this.glitchAmplitude;}x1=this.width-this.textWidth>>1;x2=x1+x0;x3=x1-x0;this.context.clearRect(0,0,this.width,this.height);this.context.globalAlpha=this.alphaMin+((1-this.alphaMin)*Math.random());switch(this.channel){case 0:this.renderChannels(x1,x2,x3);break;case 1:this.renderChannels(x2,x3,x1);break;case 2:this.renderChannels(x3,x1,x2);break;}this.renderScanline();if(Math.floor(Math.random()*2)>1){this.renderScanline();}},renderChannels:function(x1,x2,x3){const channelYPos=(this.height/2)+(this.textSize/2);var red="rgb(255, 0, 66)";var orange="rgb(255, 123, 0)";var pink="rgb(251, 0, 255)";var cyane="rgb(0, 255, 187)";this.context.font=this.font;this.context.fillStyle=cyane;this.context.fillText(this.text,x1-(this.width*0.035),channelYPos);this.context.globalCompositeOperation=this.compOp;this.context.fillStyle=red;this.context.fillText(this.text,x2-(this.width*0.035),channelYPos);this.context.fillStyle=pink;this.context.fillText(this.text,x3-(this.width*0.035),channelYPos);},renderScanline:function(){var y=this.height*Math.random()>>0,o=this.context.getImageData(0,y,this.width,1),d=o.data,i=d.length,s=this.scanlineBase+this.scanlineRange*Math.random()>>0,x=-this.scanlineShift+this.scanlineShift*2*Math.random()>>0;while(i-->0){d[i]+=s;}this.context.putImageData(o,x,y);},resize:function(){this.width=document.documentElement.offsetWidth;this.height=window.innerHeight;if(this.canvas){this.canvas.height=this.height;document.documentElement.offsetHeight;this.canvas.width=this.width;document.documentElement.offsetWidth;this.textSize=Math.floor(this.canvas.width/4);if(this.textSize>this.height){this.textSize=Math.floor(this.canvas.height/1.5);}this.font='100 '+this.textSize+'px "1UP"';this.context.font=this.font;}}};document.onload=glitcher.init();window.onresize=glitcher.resize();})();
+(function (){
+  "use strict";
+  var textSize = 10;
+  var glitcher = {
+
+    init: function () {
+      setTimeout((function () {
+        this.canvas = document.getElementById('stage');
+        this.context = this.canvas.getContext('2d');
+
+        this.initOptions();
+        this.resize();
+        this.tick();
+      }).bind(this), 100);
+    },
+
+    initOptions: function () {
+      this.width = document.documentElement.offsetWidth;
+      this.height = document.documentElement.offsetHeight;
+
+      this.textSize = Math.floor(this.width / 4);
+
+      if (this.textSize > this.height) {
+        this.textSize = Math.floor(this.height/1.5); }
+
+        this.font = '100 ' + this.textSize + 'px "1UP"';
+        this.context.font = this.font;
+        this.text = 'A';
+        this.textWidth = (this.context.measureText(this.text)).width;
+
+        this.fps = 60;
+
+      this.channel = 0; // 0 = red, 1 = green, 2 = blue
+      this.compOp = 'lighter'; // CompositeOperation = lighter || darker || xor
+      this.phase = 0.0;
+      this.phaseStep = 0.9; //determines how often we will change channel and amplitude
+      this.amplitude = 0.0;
+      this.amplitudeBase = 4.0;
+      this.amplitudeRange = 4.0;
+      this.alphaMin = 0.9;
+      this.glitchAmplitude = 5.0;
+      this.glitchThreshold = 0.90;
+      this.scanlineBase = 40;
+      this.scanlineRange = 40;
+      this.scanlineShift = 15;
+    },
+
+    tick: function () {
+      setTimeout((function () {
+        this.phase += this.phaseStep;
+
+        if (this.phase > 1) {
+          this.phase = 0.0;
+          this.channel = (this.channel === 2) ? 0 : this.channel + 1;
+          this.amplitude = this.amplitudeBase + (this.amplitudeRange * Math.random());
+        }
+
+        this.render();
+        this.tick();
+
+      }).bind(this), 1000 / this.fps);
+    },
+
+    render: function () {
+      var x0 = this.amplitude * Math.sin((Math.PI * 2) * this.phase) >> 0,
+      x1, x2, x3;
+
+      if (Math.random() >= this.glitchThreshold) {
+        x0 *= this.glitchAmplitude;
+      }
+
+      x1 = this.width - this.textWidth >> 1;
+      x2 = x1 + x0;
+      x3 = x1 - x0;
+
+      this.context.clearRect(0, 0, this.width, this.height);
+      this.context.globalAlpha = this.alphaMin + ((1 - this.alphaMin) * Math.random());
+
+      switch (this.channel) {
+        case 0:
+        this.renderChannels(x1, x2, x3);
+        break;
+        case 1:
+        this.renderChannels(x2, x3, x1);
+        break;
+        case 2:
+        this.renderChannels(x3, x1, x2);
+        break;
+      }
+      this.renderScanline();
+      if (Math.floor(Math.random() * 2) > 1) {
+        this.renderScanline();
+          // renders a second scanline 50% of the time
+        }
+      },
+
+      renderChannels: function (x1, x2, x3) {
+        const channelYPos = (this.height / 2) + (this.textSize / 2);
+
+        var red = "rgb(255, 0, 66)";
+        var orange = "rgb(255, 123, 0)";
+        var pink = "rgb(251, 0, 255)";
+        var cyane = "rgb(0, 255, 187)";
+
+        this.context.font = this.font;
+        this.context.fillStyle = cyane;
+        this.context.fillText(this.text, x1 - (this.width*0.035), channelYPos);
+        this.context.globalCompositeOperation = this.compOp;
+        this.context.fillStyle = red;
+        this.context.fillText(this.text, x2 - (this.width*0.035), channelYPos);
+        this.context.fillStyle = pink;
+        this.context.fillText(this.text, x3 - (this.width*0.035), channelYPos);
+      },
+
+      renderScanline: function () {
+        var y = this.height * Math.random() >> 0,
+        o = this.context.getImageData(0, y, this.width, 1),
+        d = o.data,
+        i = d.length,
+        s = this.scanlineBase + this.scanlineRange * Math.random() >> 0,
+        x = -this.scanlineShift + this.scanlineShift * 2 * Math.random() >> 0;
+
+        while (i-- > 0) {
+          d[i] += s;
+        }
+
+        this.context.putImageData(o, x, y);
+      },
+
+      resize: function () {
+        this.width = document.documentElement.offsetWidth;
+        this.height = window.innerHeight;
+            // this.height = document.documentElement.offsetHeight;
+            if (this.canvas) {
+              this.canvas.height = this.height;
+              document.documentElement.offsetHeight;
+              this.canvas.width = this.width;
+              document.documentElement.offsetWidth;
+              this.textSize = Math.floor(this.canvas.width / 4);
+        // RE-sets text size based on window size
+        if (this.textSize > this.height) {
+          this.textSize = Math.floor(this.canvas.height/1.5);
+        }
+          // tries to make text fit if window is very wide, but not very tall
+          this.font = '100 ' + this.textSize + 'px "1UP"';
+          this.context.font = this.font;
+        }
+      }
+    };
+
+    document.onload = glitcher.init();
+    window.onresize = glitcher.resize();
+  // return;
+ // executes anonymous function onload
+})();
